2edf499c6a58c7eb473159b7767c7447b3f8b435
revolutionkyle <khammer4@mail.csuchico.edu>
2017-10-28 19:02:13 -0700

    Comments for algorithm.

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include<vector>
+#include<string>
+#include<iostream>
+#include<bitmap.h>
+
+using namespace std;
+void getinput();
+int averagecolor();
+int coloratpixel();
+
+
+int main()
+{ char reset;
+  string image;
+  bool validImage = image.isImage();
+      //Get users input for each individual image
+      getinput(); 
+      image.open(Image);
+      // validate each image seperately and store such images as a vector of vectors
+      // gives an error if the image is not correct
+
+
+      //set up a loop that allows the user to stop or stops them after 10 pictures
+
+
+
+      //take the average of each of the red, blue, green colors of each individual picture
+
+
+
+
+      //create a new image, that has the average of each color at each pixel.
+
+      //update the user as each loop completes.
+
+
+      //save the image as "composite-revolutionkyle."
+
+
+
+return 0;     
+}     
